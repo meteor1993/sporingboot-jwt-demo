f5fc3b0c11651fa296cf0c2e28141257db68928e
inwsy <inwsy@hotmail.com>
2020-07-31 17:19:43 +0800

    Init commit

---
+++ src/main/java/com/geekdigging/sporingbootjwtdemo/SporingbootJwtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.geekdigging.sporingbootjwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SporingbootJwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SporingbootJwtDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geekdigging/sporingbootjwtdemo/SporingbootJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekdigging.sporingbootjwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SporingbootJwtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
